3bd1ff000694ba8dd0323607fd7dd2d71b349ec8
pratap-jaihind <jaihind09920@gmail.com>
2025-07-01 12:13:35 +0530

    Just created the model file or document or say table for database

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const conversationSchema = new mongoose.Schema({
+  participants: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  ],
+  messages: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+  ],
+});
+export default conversationSchema = mongoose.model(
+  "Conversation",
+  conversationSchema
+);
---
+++ backend/models/message.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    message: { type: String, required: true },
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+export default messageSchema = mongoose.model("Message", messageSchema);
