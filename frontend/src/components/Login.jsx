218cd369ff821976d850728ce52dd5652e4e0ece
pratap-jaihind <jaihind09920@gmail.com>
2025-07-05 19:25:38 +0530

    Just created the left side bar

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -7,9 +7,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { Loader2 } from "lucide-react";
 // import { useSelector } from "react-redux";
 
-const Signup = () => {
+const Login = () => {
   const [input, setInput] = useState({
-    username: "",
     email: "",
     password: "",
   });
@@ -21,12 +20,12 @@ const Signup = () => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
 
-  const signupHandler = async (e) => {
+  const loginHandler = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
       const res = await axios.post(
-        "http://localhost:8000/api/v1/user/register",
+        "http://localhost:8000/api/v1/user/login",
         input,
         {
           headers: {
@@ -36,7 +35,7 @@ const Signup = () => {
         }
       );
       if (res.data.success) {
-        navigate("/login");
+        navigate("/");
         toast.success(res.data.message);
         setInput({
           username: "",
@@ -60,25 +59,16 @@ const Signup = () => {
   return (
     <div className="flex items-center w-screen h-screen justify-center">
       <form
-        onSubmit={signupHandler}
+        onSubmit={loginHandler}
         className="shadow-lg flex flex-col gap-5 p-8"
       >
         <div className="my-4">
           <h1 className="text-center font-bold text-xl">LOGO</h1>
           <p className="text-sm text-center">
-            Signup to see photos & videos from your friends
+            Login to see photos & videos from your friends
           </p>
         </div>
-        <div>
-          <span className="font-medium">Username</span>
-          <Input
-            type="text"
-            name="username"
-            value={input.username}
-            onChange={changeEventHandler}
-            className="focus-visible:ring-transparent my-2"
-          />
-        </div>
+
         <div>
           <span className="font-medium">Email</span>
           <Input
@@ -105,12 +95,12 @@ const Signup = () => {
             Please wait
           </Button>
         ) : (
-          <Button type="submit">Signup</Button>
+          <Button type="submit">Login</Button>
         )}
         <span className="text-center">
-          Already have an account?{" "}
-          <Link to="/login" className="text-blue-600">
-            Login
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-600">
+            Signup
           </Link>
         </span>
       </form>
@@ -118,4 +108,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default Login;
---
+++ frontend/src/components/MainLayout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import LeftSidebar from "./LeftSidebar";
+
+const MainLayout = () => {
+  return (
+    <>
+      <div>
+        <LeftSidebar />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
